188c0427136c9c78d1484e72409681979969d68a
Thon-g <minhphongth06@gmail.com>
2026-06-13 16:01:09 +0700

    feat(register_feat_api): add GenerateIdUtil

--- app/src/main/java/com/abs/app/common/util/GenerateIdUtil.java
+++ app/src/main/java/com/abs/app/common/util/GenerateIdUtil.java
@@ -1,12 +1,16 @@
 package com.abs.app.common.util;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 86d4024 (feat(register_feat_api): add GenerateIdUtil)
 import java.util.UUID;
 
 public class GenerateIdUtil {
     public static String GenerateId(){
         return UUID.randomUUID().toString();
     }
+<<<<<<< HEAD
 
     public static String GenerateId(String salt, int limit) {
         return salt + "_" + UUID.randomUUID().toString().substring(0, limit);
@@ -14,4 +18,6 @@ public class GenerateIdUtil {
 =======
 public class GenerateIdUtil {
 >>>>>>> 1491b3a (feat(register_feat_api): implement register api and handler)
+=======
+>>>>>>> 86d4024 (feat(register_feat_api): add GenerateIdUtil)
 }
