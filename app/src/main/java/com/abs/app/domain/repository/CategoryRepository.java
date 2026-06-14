f441824bffb4dc0d8769522156596094aee112ce
Thon-g <minhphongth06@gmail.com>
2026-06-14 20:53:25 +0700

    feat(category_feat_api): add category repository, implement and jpa

--- app/src/main/java/com/abs/app/domain/repository/CategoryRepository.java
+++ app/src/main/java/com/abs/app/domain/repository/CategoryRepository.java
@@ -1,4 +1,14 @@
 package com.abs.app.domain.repository;
 
+import com.abs.app.domain.entity.Category;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository {
+    List<Category> findAll();
+    Optional<Category> findById(String id);
+    Optional<Category> findByName(String name);
+    Category save(Category category);
+    void deleteById(String id);
 }
--- app/src/main/java/com/abs/app/infrastructure/persistence/jpa/CategoryJpaRepository.java
+++ app/src/main/java/com/abs/app/infrastructure/persistence/jpa/CategoryJpaRepository.java
@@ -1,4 +1,10 @@
 package com.abs.app.infrastructure.persistence.jpa;
 
-public interface CategoryJpaRepository {
+import com.abs.app.domain.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryJpaRepository extends JpaRepository<Category, String> {
+    Optional<Category> findByName(String name);
 }
