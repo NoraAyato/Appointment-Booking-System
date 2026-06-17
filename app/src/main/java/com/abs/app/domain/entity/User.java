86cf94abeb832e8654b1a7e0016d10b69c3711c1
MinhHoang <hoang2942004.2@gmail.com>
2026-06-17 15:09:44 +0700

    Resolve conflict in User.java and update code

--- app/src/main/java/com/abs/app/domain/entity/User.java
+++ app/src/main/java/com/abs/app/domain/entity/User.java
@@ -6,15 +6,7 @@ import java.util.List;
 
 import com.abs.app.domain.entity.enums.RoleEnum;
 import com.abs.app.domain.entity.enums.UserStatus;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
