9fce98fabdc4b9437cafa7ef8382be9c92610974
Thon-g <minhphongth06@gmail.com>
2026-06-17 20:40:33 +0700

    feat(service_feat_api): setup domain, infrastructure and shared dtos for service

---
+++ app/src/main/java/com/abs/app/infrastructure/mapper/ServiceMapper.java
@@ -0,0 +1,35 @@
+package com.abs.app.infrastructure.mapper;
+
+import com.abs.app.application.admin.service.dto.ServiceImageDto;
+import com.abs.app.application.admin.service.dto.ServiceResponseDto;
+import com.abs.app.domain.entity.ServiceEntity;
+import com.abs.app.domain.entity.ServiceImage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceMapper {
+    public static ServiceResponseDto toServiceResponse(ServiceEntity service) {
+        ServiceResponseDto dto = new ServiceResponseDto();
+        dto.setId(service.getId());
+        dto.setName(service.getName());
+        dto.setDescription(service.getDescription());
+        dto.setDurationMinutes(service.getDurationMinutes());
+        dto.setPrice(service.getPrice());
+
+        if (service.getCategory() != null) {
+            dto.setCategoryName(service.getCategory().getName());
+        }
+
+        if (service.getServiceImage() != null && !service.getServiceImage().isEmpty()) {
+            List<ServiceImageDto> imageDtoList = new ArrayList<>();
+            service.getServiceImage().stream().toList().forEach(img -> {
+                ServiceImageDto imageDto = new ServiceImageDto(img.getPicture(), img.getIsMainImage());
+                imageDtoList.add(imageDto);
+            });
+            dto.setServiceImageList(imageDtoList);
+        }
+
+        return dto;
+    }
+}
