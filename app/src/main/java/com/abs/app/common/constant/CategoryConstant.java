69b8ad5156f4b5590e6f448fd47b07d35bc17638
Thon-g <minhphongth06@gmail.com>
2026-06-22 15:31:23 +0700

    refactor(category_feat_api): add validation constant from CategoryConstant and apply it to category request dto

--- app/src/main/java/com/abs/app/common/constant/CategoryConstant.java
+++ app/src/main/java/com/abs/app/common/constant/CategoryConstant.java
@@ -1,6 +1,7 @@
 package com.abs.app.common.constant;
 
 public class CategoryConstant {
+    // Exception
     public static final String CREATE_SUCCESS = "Tạo mới Category thành công";
     public static final String GET_SUCCESS = "Thành công danh sách Category";
     public static final String UPDATE_SUCCESS = "Cập nhật Category thành công";
@@ -11,4 +12,8 @@ public class CategoryConstant {
     public static final String SALT_TAG = "ca";
     public static final int STRING_LIMIT = 10;
 
+    // Validation
+    public static final String VALID_NAME_NOT_BLANK = "Tên danh mục không được để trống.";
+    public static final String VALID_NAME_SIZE = "Tên danh mục phải từ {min} đến {max} ký tự.";
+    public static final String VALID_DESCRIPTION_NOT_BLANK = "Mô tả danh mục không được để trống.";
 }
