b5aea027594207fd086a2490933c7f78c2bc442f
NoraAyato <laithanhtrunghutech@gmail.com>
2026-06-25 15:41:45 +0700

    feat(user_feat_api): add constant for messages and userconstant

--- app/src/main/java/com/abs/app/common/constant/UserConstant.java
+++ app/src/main/java/com/abs/app/common/constant/UserConstant.java
@@ -3,4 +3,7 @@ package com.abs.app.common.constant;
 public class UserConstant {
     public static String USER_NOT_EXIST = "Người dùng không tồn tại";
     public static String USER_ALREADY_EXIST = "Người dùng đã tồn tại";
+    public static String GET_USER_INFO_SUCCESS = "Lấy thông tin người dùng thành công";
+    public static String UPDATE_USER_INFO_SUCCESS = "Cập nhật thông tin người dùng thành công";
+    public static String UPDATE_USER_IMAGE_SUCCESS = "Cập nhật ảnh đại diện thành công";
 }
