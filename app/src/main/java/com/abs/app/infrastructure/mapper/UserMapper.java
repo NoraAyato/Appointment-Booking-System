5a0f1b00907ad28b7b409d612cd36c21580278f8
NoraAyato <laithanhtrunghutech@gmail.com>
2026-06-27 12:00:42 +0700

    feat(user_manager_feat_api): implment query handler , response dto , mapper for get user pagination list

--- app/src/main/java/com/abs/app/infrastructure/mapper/UserMapper.java
+++ app/src/main/java/com/abs/app/infrastructure/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.abs.app.infrastructure.mapper;
 
+import com.abs.app.application.admin.usermanager.dto.UserResponseDto;
 import com.abs.app.application.user.dto.UserInfoResponeDto;
 import com.abs.app.domain.entity.User;
 
@@ -17,5 +18,17 @@ public class UserMapper {
                 user.getRole().getRoleName().toString(),
                 user.isGender(),
                 user.isRecieveEmail());
+    }
+
+    public static UserResponseDto toUserResponseDto(User user) {
+        return new UserResponseDto(
+                user.getUserId(),
+                user.getUserName(),
+                user.getEmail(),
+                user.getPhoneNumber(),
+                user.getRole().getRoleName().toString(),
+                user.getStatus().name(),
+                user.getCreatedAt().toString(),
+                user.getPicture());
     }
 }
