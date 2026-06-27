f04c30a91aa31bf0f0f2209702f3d71f88c1f4c4
NoraAyato <laithanhtrunghutech@gmail.com>
2026-06-27 12:05:01 +0700

    feat(user_manager_feat_api): add constant for user

--- app/src/main/java/com/abs/app/common/constant/UserConstant.java
+++ app/src/main/java/com/abs/app/common/constant/UserConstant.java
@@ -6,4 +6,8 @@ public class UserConstant {
     public static String GET_USER_INFO_SUCCESS = "Lấy thông tin người dùng thành công";
     public static String UPDATE_USER_INFO_SUCCESS = "Cập nhật thông tin người dùng thành công";
     public static String UPDATE_USER_IMAGE_SUCCESS = "Cập nhật ảnh đại diện thành công";
+    public static String GET_USER_LIST_SUCCESS = "Lấy danh sách người dùng thành công";
+    public static String GET_USER_STATS_SUCCESS = "Lấy thống kê người dùng thành công";
+    public static String INVALID_USER_STATUS = "Trạng thái không hợp lệ";
+
 }
