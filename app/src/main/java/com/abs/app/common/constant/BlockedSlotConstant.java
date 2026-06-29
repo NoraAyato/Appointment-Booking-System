a8e25cb1d0a93bb8d4c0559e9e0371ea3b168b31
NoraAyato <laithanhtrunghutech@gmail.com>
2026-06-29 12:36:28 +0700

    feat(blocked_slot_manager_feat_api): add constant for blocked slot

---
+++ app/src/main/java/com/abs/app/common/constant/BlockedSlotConstant.java
@@ -0,0 +1,5 @@
+package com.abs.app.common.constant;
+
+public class BlockedSlotConstant {
+    public static final String GET_STAFF_BLOCKED_SLOTS_SUCCESS = "Lấy danh sách các khung giờ bị chặn của nhân viên thành công";
+}
