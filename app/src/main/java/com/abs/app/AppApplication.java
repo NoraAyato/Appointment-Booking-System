43dc460d89f541ae2d4542b298fcfbefa6983edb
NoraAyato <laithanhtrunghutech@gmail.com>
2026-06-29 14:56:17 +0700

    config(blocked_slot_manager_feat_api): config timezone for vietnam

--- app/src/main/java/com/abs/app/AppApplication.java
+++ app/src/main/java/com/abs/app/AppApplication.java
@@ -1,5 +1,7 @@
 package com.abs.app;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(AppApplication.class, args);
 	}
-
 }
