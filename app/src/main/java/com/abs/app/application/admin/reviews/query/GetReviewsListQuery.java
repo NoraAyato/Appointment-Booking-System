f7a6d43f087fe6360a461617488b6d31b6989763
phanminhson2204-code <phanminhson2204@gmail.com>
2026-06-30 22:54:13 +0700

    refactor(reviews_manager_feat_api):  query for fetching reviews list

--- app/src/main/java/com/abs/app/application/admin/reviews/query/GetReviewsListQuery.java
+++ app/src/main/java/com/abs/app/application/admin/reviews/query/GetReviewsListQuery.java
@@ -1,11 +1,11 @@
-package com.abs.app.application.admin.reviewmanager.query;
+package com.abs.app.application.admin.reviews.query;
 
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetReviewListQuery {
+public class GetReviewsListQuery {
     private String keyWord;
     private String status;
     private int page;
--- app/src/main/java/com/abs/app/application/admin/reviews/query/GetReviewsListQueryHandler.java
+++ app/src/main/java/com/abs/app/application/admin/reviews/query/GetReviewsListQueryHandler.java
@@ -1,26 +1,23 @@
-package com.abs.app.application.admin.reviewmanager.query;
+package com.abs.app.application.admin.reviews.query;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
-import com.abs.app.application.admin.reviewmanager.dto.ReviewResponseDto;
+import com.abs.app.application.admin.reviews.dto.ReviewsResponseDto;
 import com.abs.app.common.response.PageResponse;
-import com.abs.app.common.util.PaginationUtil;
 import com.abs.app.domain.entity.Reviews;
-import com.abs.app.domain.entity.User;
-import com.abs.app.domain.repository.ReviewRepository;
-import com.abs.app.infrastructure.mapper.ReviewMapper;
+import com.abs.app.domain.repository.ReviewsRepository;
+import com.abs.app.infrastructure.mapper.ReviewsMapper;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class GetReviewListQueryHandler {
-    private final ReviewRepository reviewsRepository;
-    public PageResponse<ReviewResponseDto> handle(GetReviewListQuery query){
+public class GetReviewsListQueryHandler {
+    private final ReviewsRepository reviewsRepository;
+    public PageResponse<ReviewsResponseDto> handle(GetReviewsListQuery query){
         List<Reviews> reviews = reviewsRepository.findAll();
         List<Reviews> filterReviews = reviews.stream()
                 .filter(item ->(query.getKeyWord() == null
@@ -30,8 +27,8 @@ public class GetReviewListQueryHandler {
                         item.getStatus().name().equalsIgnoreCase(query.getStatus())))
                 .collect(Collectors.toList());
         int total = filterReviews.size();
-        List<ReviewResponseDto> items = filterReviews.stream()
-                .map(ReviewMapper::toAdminReviewResponseDto)
+        List<ReviewsResponseDto> items = filterReviews.stream()
+                .map(ReviewsMapper::toAdminReviewResponseDto)
                 .collect(Collectors.toList());
         int page = query.getPage();
         int limit = query.getLimit();
