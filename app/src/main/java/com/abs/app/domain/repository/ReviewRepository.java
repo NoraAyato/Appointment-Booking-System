c59e53a4fce6f36925ecd40bd8d29f6f25aa7c83
MinhHoang <hoang2942004.2@gmail.com>
2026-06-30 15:10:43 +0700

    feat(reviews_manager_feat_api): implement review repository interface

---
+++ app/src/main/java/com/abs/app/domain/repository/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.abs.app.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.abs.app.domain.entity.Reviews;
+
+public interface ReviewRepository {
+    List<Reviews> findAll();
+    
+    Optional<Reviews> findById(String id);
+    
+    Reviews save(Reviews review);
+}
