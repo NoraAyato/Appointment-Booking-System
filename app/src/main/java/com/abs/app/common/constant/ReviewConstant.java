730f744119be2c544a6cf16d3cd8a35ba36e06e4
MinhHoang <hoang2942004.2@gmail.com>
2026-06-30 15:10:40 +0700

    feat(reviews_manager_feat_api): add review constant messages

---
+++ app/src/main/java/com/abs/app/common/constant/ReviewConstant.java
@@ -0,0 +1,6 @@
+package com.abs.app.common.constant;
+
+public class ReviewConstant {
+    public static final String GET_REVIEW_LIST_SUCCESS = "Get review list successfully";
+    public static final String UPDATE_REVIEW_STATUS_SUCCESS = "Update review status successfully";
+}
