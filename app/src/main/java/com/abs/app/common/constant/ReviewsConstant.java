a321beb5a794485087d4a15baeae86a3c58dfb9b
phanminhson2204-code <phanminhson2204@gmail.com>
2026-07-01 00:33:13 +0700

    refactor(constant): update reviews constant messages

--- app/src/main/java/com/abs/app/common/constant/ReviewsConstant.java
+++ app/src/main/java/com/abs/app/common/constant/ReviewsConstant.java
@@ -5,4 +5,5 @@ public class ReviewsConstant {
     public static final String GET_REVIEW_SLOTS_SUCCESS = "Lấy danh sách các các đánh giá thành công";
     public static final String REVIEW_NOT_EXIST = "Đánh giá không tồn tại";
     public static final String UPDATE_REVIEW_SUCCESS = "Cập nhật đánh giá thành công";
+
 }
