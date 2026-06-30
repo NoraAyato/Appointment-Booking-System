61ccf7b7c4a6605f75a2dcc1758ffc7bcfcf933e
MinhHoang <hoang2942004.2@gmail.com>
2026-06-30 15:10:40 +0700

    feat(reviews_manager_feat_api): update review entity with status

--- app/src/main/java/com/abs/app/domain/entity/Reviews.java
+++ app/src/main/java/com/abs/app/domain/entity/Reviews.java
@@ -6,11 +6,14 @@ import java.time.LocalDateTime;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import com.abs.app.domain.entity.enums.ReviewStatus;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,6 +36,10 @@ public class Reviews {
 
     @Column(name = "create_at", nullable = false, updatable = false)
     private LocalDateTime createAt = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private ReviewStatus status = ReviewStatus.PENDING;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "appointment_id")
