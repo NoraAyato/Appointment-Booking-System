36d2a01e9c29e3bb8bf7824d129b38f1b0490977
MinhHoang <hoang2942004.2@gmail.com>
2026-06-30 15:10:41 +0700

    feat(reviews_manager_feat_api): implement review response dto

---
+++ app/src/main/java/com/abs/app/application/admin/reviewmanager/dto/ReviewResponseDto.java
@@ -0,0 +1,23 @@
+package com.abs.app.application.admin.reviewmanager.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.abs.app.domain.entity.enums.ReviewStatus;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ReviewResponseDto {
+    private String id;
+    private String picture;
+    private String description;
+    private BigDecimal serviceScore;
+    private ReviewStatus status;
+    private LocalDateTime createAt;
+    private String customerName;
+}
