e9f3c055a538ea2d2a716083017ccb7e2d80229e
Thon-g <minhphongth06@gmail.com>
2026-07-01 14:21:48 +0700

    feat(staff_manager_feat_api): implement create staff service command and request dto

---
+++ app/src/main/java/com/abs/app/application/admin/staffservice/dto/CreateStaffServiceRequestDto.java
@@ -0,0 +1,9 @@
+package com.abs.app.application.admin.staffservice.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateStaffServiceRequestDto {
+    private String staffId;
+    private String serviceId;
+}
