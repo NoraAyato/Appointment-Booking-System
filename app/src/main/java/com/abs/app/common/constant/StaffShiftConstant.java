a664e59863e3913d9d9cb5b1e0443cbc5ca7363f
Thon-g <minhphongth06@gmail.com>
2026-07-01 18:59:30 +0700

    feat(staff_manager_feat_api): add staff shift constant

--- app/src/main/java/com/abs/app/common/constant/StaffShiftConstant.java
+++ app/src/main/java/com/abs/app/common/constant/StaffShiftConstant.java
@@ -1,4 +1,14 @@
 package com.abs.app.common.constant;
 
 public class StaffShiftConstant {
+    public static final String STAFF_SHIFT_NOT_FOUND = "Không tồn tại ca làm việc";
+    public static final String ONLY_STAFF_ALLOWED = "Ca làm việc chỉ dành cho nhân viên";
+    public static final String WORK_TIME_OVERLAP = "Khung giờ làm việc đã tồn tại trong thời gian này";
+    public static final String WORK_TIME_BLOCKED = "Không thể tạo ca làm việc trong thời gian nghỉ";
+    public static final String GET_LIST_SUCCESS = "Lấy danh sách ca làm việc của nhân viên thành công";
+    public static final String CREATE_SUCCESS = "Tạo ca làm việc thành công";
+
+    public static final String VALID_WORK_DATE = "Ngày làm việc không hợp lệ";
+    public static final String VALID_START_TIME = "Thời gian bắt đầu ca làm không hợp lệ";
+    public static final String VALID_END_TIME = "Thời gian kết thúc ca làm không hợp lệ";
 }
