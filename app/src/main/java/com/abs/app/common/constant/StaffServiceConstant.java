e3b33345def2244eaf9dd143e7cf37f4db7a5ec8
Thon-g <minhphongth06@gmail.com>
2026-07-01 14:15:15 +0700

    feat(staff_manager_feat_api): add staff service constant

---
+++ app/src/main/java/com/abs/app/common/constant/StaffServiceConstant.java
@@ -0,0 +1,11 @@
+package com.abs.app.common.constant;
+
+public class StaffServiceConstant {
+    public static final String CREATE_SUCCESS = "Tạo mới StaffService thành công";
+    public static final String NOT_EXIST = "StaffService không tồn tại";
+    public static final String DELETE_SUCCESS = "Xóa StaffService thành công";
+    public static final String STAFF_NOT_ASSIGNED_SERVICE = "Nhân viên chưa được gán dịch vụ này";
+    public static final String SERVICE_ALREADY_DELETED_FROM_STAFF = "Dịch vụ này đã bị xóa khỏi nhân viên từ trước";
+    public static final String STAFF_ALREADY_RESPONSIBLE_SERVICE = "Nhân viên đã phụ trách dịch vụ này rồi";
+    public static final String USER_NOT_STAFF = "User này không phải là nhân viên";
+}
