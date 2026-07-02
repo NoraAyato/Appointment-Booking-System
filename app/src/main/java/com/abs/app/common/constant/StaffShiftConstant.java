4406caf5a25d15f11dc03132dff10a1a4093703d
Thon-g <minhphongth06@gmail.com>
2026-07-02 09:44:08 +0700

    refactor(staff_manager_feat_api): add valid constant for StaffShiftConstant

--- app/src/main/java/com/abs/app/common/constant/StaffShiftConstant.java
+++ app/src/main/java/com/abs/app/common/constant/StaffShiftConstant.java
@@ -7,8 +7,12 @@ public class StaffShiftConstant {
     public static final String WORK_TIME_BLOCKED = "Không thể tạo ca làm việc trong thời gian nghỉ";
     public static final String GET_LIST_SUCCESS = "Lấy danh sách ca làm việc của nhân viên thành công";
     public static final String CREATE_SUCCESS = "Tạo ca làm việc thành công";
+    public static final String UPDATE_SUCCESS = "Cập nhật ca làm việc thành công";
+    public static final String INVALID_BLOCKED_SLOT_STATUS = "Trạng thái ca làm việc không hợp lệ";
 
     public static final String VALID_WORK_DATE = "Ngày làm việc không hợp lệ";
     public static final String VALID_START_TIME = "Thời gian bắt đầu ca làm không hợp lệ";
     public static final String VALID_END_TIME = "Thời gian kết thúc ca làm không hợp lệ";
+    public static final String VALID_STAFF_ID = "ID nhân viên không được để trống";
+    public static final String VALID_STATUS = "Trạng thái ca làm không được để trống";
 }
