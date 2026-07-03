fc6fc46ba35c71bdba5216840208ff64863b1864
NoraAyato <laithanhtrunghutech@gmail.com>
2026-07-03 13:08:59 +0700

    feat(category_feat_api): implement get category option query and dto response

---
+++ app/src/main/java/com/abs/app/application/user/category/dto/CategoryOptionResponse.java
@@ -0,0 +1,13 @@
+package com.abs.app.application.user.category.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryOptionResponse {
+    private String id;
+    private String name;
+}
