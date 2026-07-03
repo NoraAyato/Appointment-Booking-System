7220ae6a312da71662a5e6984e1eab5c379e2502
NoraAyato <laithanhtrunghutech@gmail.com>
2026-07-03 13:10:47 +0700

    feat(category_feat_api): add mapper for category option

--- app/src/main/java/com/abs/app/infrastructure/mapper/CategoryMapper.java
+++ app/src/main/java/com/abs/app/infrastructure/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package com.abs.app.infrastructure.mapper;
 
 import com.abs.app.application.admin.category.dto.CategoryResponseDto;
+import com.abs.app.application.user.category.dto.CategoryOptionResponse;
 import com.abs.app.domain.entity.Category;
 
 public class CategoryMapper {
@@ -10,7 +11,13 @@ public class CategoryMapper {
         Dto.setName(category.getName());
         Dto.setDescription(category.getDescription());
         Dto.setTotalService(category.getServices() != null ? category.getServices().size() : 0);
+        return Dto;
+    }
 
+    public static CategoryOptionResponse toCategoryOptionResponse(Category category) {
+        CategoryOptionResponse Dto = new CategoryOptionResponse();
+        Dto.setId(category.getId());
+        Dto.setName(category.getName());
         return Dto;
     }
 }
