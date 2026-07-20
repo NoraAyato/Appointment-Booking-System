4b4986ba0bbf1bd79b982b325c8f121b9777eff9
Thon-g <minhphongth06@gmail.com>
2026-07-20 23:33:49 +0700

    fix(feat/staff_manager_api): check Service status before creating staff service

--- app/src/main/java/com/abs/app/common/constant/StaffServiceConstant.java
+++ app/src/main/java/com/abs/app/common/constant/StaffServiceConstant.java
@@ -9,6 +9,7 @@ public class StaffServiceConstant {
     public static final String SERVICE_ALREADY_DELETED_FROM_STAFF = "Dịch vụ này đã bị xóa khỏi nhân viên từ trước";
     public static final String STAFF_ALREADY_RESPONSIBLE_SERVICE = "Nhân viên đã phụ trách dịch vụ này rồi";
     public static final String USER_NOT_STAFF = "User này không phải là nhân viên";
+    public static final String NOT_RESPONSIBLE_INACTIVE_SERVICE = "Không thể phân công dịch vụ đã ngừng hoạt động";
 
     public static final String VALID_STAFF_ID_REQUIRED = "StaffId không được để trống";
     public static final String VALID_SERVICE_ID_REQUIRED = "ServiceId không được để trống";
